ed117e5e0302f1fb9d11ac392a4965c56c381cd5
Ricardo Ferreira <ricardoferreirafatec@yahoo.com.br>
2025-02-08 12:37:09 -0300

    Inicio 08-02-2025
    Primeira aula 08-02-2025

---
+++ RicardoPythonSabado/exerc1.py
@@ -0,0 +1,13 @@
+#soma de 3 números
+print("seja bem vindo ao inseridor de numeros")
+print("digita um número")
+n1 = input()
+
+print("digite outro numero")
+n2 = input()
+
+print("digite o terceiro numero")
+n3 = input()
+
+resultado = n1+n2+n3
+print("esse é o seu recultado:" + resultado)
---
+++ RicardoPythonSabado/helloworld.py
@@ -0,0 +1,7 @@
+# comentario de uma linha
+# cntrl + s
+""" 
+alt+shift+a
+Escrever o comentário em duas linhas
+"""
+print("Hello World")
