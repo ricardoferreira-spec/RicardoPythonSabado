8f4f2c61c95ba2759aca889ced7272a8a37e3851
Ricardo Ferreira <ricardoferreirafatec@yahoo.com.br>
2025-03-22 11:58:19 -0300

    22-03-25
    22-03-25

--- aula4/sistema_mysql/src/controller/produto_controller.py
+++ aula4/sistema_mysql/src/controller/produto_controller.py
@@ -1,36 +1,37 @@
-from model.produto_model import ProdutoModel
+from ..model.produto_model import ProdutoModel
+
 
 def listar_produtos():
-    """Retorna a lista de todos os produtos(dict)"""
     model = ProdutoModel()
     produtos = model.get_all_products()
-    model.close_coneection()
+    model.close_connection()
     return produtos
 
 def cadastrar_produto(nome, preco):
-    """" Cadastrar produto no banco"""
     model = ProdutoModel()
     novo_id = model.insert_product(nome, preco)
-    model.close_coneection()
+    model.close_connection()
     return novo_id
 
 def atualizar_produto(produto_id, nome, preco):
     model = ProdutoModel()
     linhas_afetadas = model.update_product_by_id(produto_id, nome, preco)
-    model.close_coneection()
+    model.close_connection()
     return linhas_afetadas
 
-
 def remover_produto(produto_id):
-    model = ProdutoModel
+    model = ProdutoModel()
     linhas_afetadas = model.delete_product_by_id(produto_id)
-    model.close_coneection()
+    model.close_connection()
     return linhas_afetadas
 
 def obter_produto(produto_id):
     model = ProdutoModel()
     produto = model.get_product_by_id(produto_id)
-    model.close_coneection()
+    model.close_connection()
     return produto
 
-    
+
+
+
+
